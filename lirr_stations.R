75e404051863611575dc19f0f6109179f8f0cfba
Katie <katie.segreti@gmail.com>
2019-03-15 16:08:11 -0400

    Update lirr_stations.R

--- lirr_stations.R
+++ lirr_stations.R
@@ -1,19 +1,21 @@
 library(tidyverse)
 library(rvest)
 library(purrr)
-library(pdftools)
 library(forcats)
 library(rebus)
+library(geosphere)
+library(tidyr)
 
-lirr_url <- "http://lirr42.mta.info/"
-
-stations <- c(read_html(lirr_url) %>%
+#url for list of stations
+lirr_url_0 <- "http://lirr42.mta.info/stations.php"
+#get list of all station urls
+stations <- c(read_html(lirr_url_0) %>%
                 html_nodes('#leftcolumn a') %>%
                 html_attr("href"),
-              read_html(lirr_url) %>%
+              read_html(lirr_url_0) %>%
                 html_nodes('#middlecolumn a') %>%
                 html_attr("href"),
-              read_html(lirr_url) %>%
+              read_html(lirr_url_0) %>%
                 html_nodes('#middlecolumn2 a') %>%
                 html_attr("href")) %>%
   str_replace_na() 
@@ -25,7 +27,7 @@ is_station <- str_detect(stations, "stationInfo")
 stations <- stations[is_station]
 
 
-
+lirr_url <- "http://lirr42.mta.info/"
 
 lirr_stations <- c()
 for (k in 1:length(stations)) {
@@ -46,7 +48,7 @@ for (k in 1:length(stations)) {
 }
 #pattern to extract the fare zone
 fare <- "Fare Zone " %R% one_or_more(DGT)
-
+#pattern to keep just the branch
 branch_pattern <- ".*Branch"
 
 lirr_stations1 <- lirr_stations %>%
@@ -63,17 +65,31 @@ lirr_stations1 <- lirr_stations %>%
 #   html_node('table') %>%
 #   html_table(header = TRUE)
 
+#create fares dataframe
 zone <- sort(unique(lirr_stations1$fare_zone))
 monthly_ticket <- c(190, 226, 261, 297, 350, 391, 461, 500)
 monthly_fares <- data.frame(zone, monthly_ticket)
 
+#join fares to stations
 lirr_stations1 <- lirr_stations1 %>%
   left_join(monthly_fares, by = c(fare_zone = "zone"))
 
+#patterns for getting latitude and longitude
+lat_lon1 <- "ll=" %R% one_or_more(DGT) %R% "." %R% one_or_more(DGT) %R% ",-" %R% one_or_more(DGT) %R% "." %R% one_or_more(DGT)
+lat_lon2 <- "2d-" %R% one_or_more(DGT) %R% "." %R% one_or_more(DGT) %R% "!3d" %R% one_or_more(DGT) %R% "." %R% one_or_more(DGT)
 
 #get latitude and longitude from the map url
 lirr_stations1 <- lirr_stations1 %>%
-  mutate(l_l = str_extract(location, lat_lon))
+  mutate(location_coords = ifelse(str_detect(location, "maps.google"),
+                                  str_extract(location, lat_lon1),
+                                  str_extract(location, lat_lon2)) %>%
+           str_remove("ll=") %>%
+           str_remove("2d") %>%
+           str_remove("3d") %>%
+           str_replace("!", ",")) 
 
+#assign penn station latitude and longitude
+penn_lat <- 40.750053
+penn_long <- -73.992362
 
-str_extract(lirr_stations1$location[5], lat_lon)
+#separate latitude and longitude
