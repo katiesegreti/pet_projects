d3ed03ea5c231f9392fb3214b6ffb8daa184b5cd
Katie <katie.segreti@gmail.com>
2020-12-14 11:19:26 -0500

    Update mashup_names_app.R

--- mashup_names_app.R
+++ mashup_names_app.R
@@ -33,35 +33,43 @@ first_n_pattern <- function(name1, n) {
   }
 }
 #make names by calling the pattern functions
+#make names by calling the pattern functions
 pattern_names <- function(startname, first_or_last, n) {
   startname <- tolower(startname)
   if(first_or_last == "first") {
     pttrn = first_n_pattern(startname, n)
     match_names = str_subset(all_names, pattern = pttrn)
     names_splt = str_split(match_names, pattern = pttrn, simplify = TRUE)
-    name_start = names_splt[,1]
+    if(length(names_splt) > 0) {name_start = names_splt[,1]
     name_start = names_splt[str_length(name_start) > 0, 1]
-    return(capitalize(unique(str_c(name_start, startname))))
+    return(capitalize(unique(str_c(name_start, startname))))}
   } else if(first_or_last == "last") {
     pttrn = last_n_pattern(startname, n)
     match_names = str_subset(all_names, pattern = pttrn)
     names_splt = str_split(match_names, pattern = pttrn, simplify = TRUE)
-    name_end = names_splt[,2]
+    if(length(names_splt > 0)) {name_end = names_splt[,2]
     name_end = names_splt[str_length(name_end) > 0, 2]
     return(capitalize(unique(str_c(startname, name_end))))
+    }
   }
 }
 #curate names depending on name length
 mashup_names <- function(startname) {
   if(str_length(startname) > 4) {
-    unique(c(pattern_names(startname, "last", 4), pattern_names(startname, "last", 3),
-             pattern_names(startname, "first", 4), pattern_names(startname, "first", 3)))
+    results <- unique(c(pattern_names(startname, "last", 4), pattern_names(startname, "last", 3),
+                        pattern_names(startname, "first", 4), pattern_names(startname, "first", 3)))
   }
   else if(str_length(startname) > 3){
-    unique(c(pattern_names(startname, "last", 3), pattern_names(startname, "first", 3)))
+    results <- unique(c(pattern_names(startname, "last", 3), pattern_names(startname, "first", 3)))
   }
   else if(str_length(startname) > 2) {
-    unique(c(pattern_names(startname, "last", 2), pattern_names(startname, "first", 2)))
+    results <- unique(c(pattern_names(startname, "last", 2), pattern_names(startname, "first", 2)))
+  }
+  if(length(results) == 0) {
+    return("Sorry, I didn't find any names to mash that up with! Try a different name?")
+  }
+  else {
+    return(results)
   }
 } 
 
@@ -76,7 +84,7 @@ font-size: 20px;
 
 #UI
 ui <- fluidPage(
-  tags$style(my_css),
+  shiny::tags$style(my_css),
   theme = shinytheme("united"),
   
   # App title
@@ -84,6 +92,8 @@ ui <- fluidPage(
   p("Enter a name and 
                 I'll combine it with other names 
                 that have similar letter patterns"),
+  a(href = "http://www.data-chips.com/", "data chips home"),
+  br(),
   sidebarLayout(
     #Input
     sidebarPanel(
@@ -95,24 +105,23 @@ ui <- fluidPage(
       # Action button get mashups
       actionButton(inputId = "get_mashups", 
                    label = "Get mashups")
+      
     ),
+    
     #Output
     mainPanel(
       h3("Mashups:"),
       verbatimTextOutput("result")
+      
     )
     )
   )
 #Server
 server <- function(input, output) {
-  # name_results <- reactive({
-  #   mashup_names(input$og_name)
-  # })
-
   name_results <- eventReactive(
     eventExpr = input$get_mashups,
     valueExpr = { mashup_names(input$og_name) },
-    ignoreNULL = FALSE
+    ignoreNULL = TRUE
   )
   output$result <- renderPrint({
     name_results()
